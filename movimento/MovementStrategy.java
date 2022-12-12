53147745e221fd7c3b7f6185a7ee399e38c5e1ad
Marcos Antônio Baccin <74544990+marcosbaccin@users.noreply.github.com>
2022-12-12 15:46:29 -0300

    Delete movimento directory

--- movimento/MovementStrategy.java
+++
@@ -1,9 +0,0 @@
-/*Algorithm by MACE*/
-package prototipoJogo2.movimento;
-
-import prototipoJogo2.warrior.Warrior;
-/*Interface para o nosso Strategy(referente aos movimentos do personagem).*/
-public interface MovementStrategy {
-
-	void execute(Warrior w);
-}
